db63957e8a8ac6ca6d4cb312b02a5d15b86da2b4
Sikander Singh <34490272+sikander441@users.noreply.github.com>
2018-01-04 00:45:11 +0530

    Update Tester.py

--- Tester.py
+++ Tester.py
@@ -1,2 +1,2 @@
-# The following is a Python 3 code that will be used to tet functions written in Funcs.py
+# The following is a Python 3 code that will be used to test functions written in Funcs.py
 # The main function is the function to run for appropriate outputs.
