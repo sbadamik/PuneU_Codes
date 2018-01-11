ba494ed314bed5cf09bebb3df01bcc3ad142f099
sbadamik <sbadamik@purdue.edu>
2018-01-11 11:55:04 +0530

    Tester now contains test for writing to a new .xyz file

--- Tester.py
+++ Tester.py
@@ -1,2 +1,29 @@
-# The following is a Python 3 code that will be used to tet functions written in Funcs.py
+# The following is a Python 3 code that will be used to test functions written in Funcs.py
 # The main function is the function to run for appropriate outputs.
+
+import Funcs
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+# # Testing the writing of XYZ
+# Locations = [[1,1,0],[2,2,0],[3,3,0],[4,4,0]]
+
+# Clearance = open("FirstText.xyz","w")
+# Clearance.close();
+
+# Funcs.WriteXYZ("FirstText.xyz",Locations)
+# Locations[3][2] = 1
+# Locations[3][1] = 5
+# Locations[0][0] = 3
+# Locations[0][1] = 3
+# Locations[1][2] = 1
+# Locations[1][0] = 4
+# Locations[2][1] = 4
+# Locations[2][0] = 4
+# Funcs.WriteXYZ("FirstText.xyz",Locations)
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+
+Pos = Funcs.Read_XYZ("TestXYZ_3Val.xyz")
+
+print(Pos)
